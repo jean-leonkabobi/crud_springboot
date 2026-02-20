a8661ef625c81f1feab6575d1adff6737ffd637f
JeanLeon KABOBI <jeanleon.kabobi@gmail.com>
2026-02-20 00:37:46 +0000

    Initial commit: Spring Boot CRUD application for Person management

---
+++ src/main/java/com/jlk/crud_springboot/PersonController.java
@@ -0,0 +1,65 @@
+package com.jlk.crud_springboot;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons() {
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        Person personCreated = personRepository.save(person);
+        return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable Long id){
+        Optional<Person> person = personRepository.findById(id);
+
+        if(person.isPresent()) {
+            return new ResponseEntity<>(person.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person personDetails){
+
+        Optional<Person> person = personRepository.findById(id);
+        if (person.isPresent()){
+            Person existingPerson = person.get();
+            existingPerson.setCity(personDetails.getCity());
+            existingPerson.setPhoneNumber(personDetails.getPhoneNumber());
+
+            Person updatedPerson = personRepository.save(existingPerson);
+            return new ResponseEntity<>(updatedPerson, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Person> deletePerson(@PathVariable Long id){
+        Optional<Person> person = personRepository.findById(id);
+
+        if (person.isPresent()){
+            personRepository.delete(person.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/jlk/crud_springboot/PersonRepository.java
@@ -0,0 +1,6 @@
+package com.jlk.crud_springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
